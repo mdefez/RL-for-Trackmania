0742035417796a49309aa07381aa0d0c473d58b9
mdefez <max.defez@gmail.com>
2026-02-18 19:21:03 +0100

    benchmark ready

--- setup/main.py
+++ setup/main.py
@@ -23,6 +23,7 @@ sys.path.append("..")
 from Learning.models.naive import NaiveModel
 from Learning.environments.naive.naive_env import NaiveEnv
 
+
 env = NaiveEnv(delta_t = 0.1)       # Temps en secondes
-model = NaiveModel(env, weights_path=None)  
+model = NaiveModel(env, weights_path=False) # "../Learning/models/with_time.pth"  
 model.learn(total_timesteps=int(1e5))
